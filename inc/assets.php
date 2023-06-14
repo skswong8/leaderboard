592d9a31a308e4bb060f2082df3650f922b1f2ee
Sam Wong <samuelwong28@gmail.com>
2023-06-14 15:19:51 +0100

    Register custom block category

--- inc/assets.php
+++ inc/assets.php
@@ -14,6 +14,7 @@ function bootstrap() : void {
     add_action('init', __NAMESPACE__ . '\\register_leaderboard_block' );
     add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\\enqueue_editor_assets' );
 	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_frontend_assets', 11 );
+    add_filter( 'block_categories_all', __NAMESPACE__ . '\\add_block_category', 10, 1 );
 }
 
 /**
@@ -65,4 +66,19 @@ function enqueue_frontend_assets() : void {
         'leaderboard-frontend-css',
         PLUGIN_BUILD . '/frontend.css'
     );
+}
+
+/**
+ * Register a custom block category for this plugin.
+ *
+ * @param array $categories The list of available block categories.
+ * @return array The filtered categories list.
+ */
+function add_block_category( array $categories ) {
+	return array_merge( $categories, [
+		[
+			'slug'  => 'custom-blocks',
+			'title' => 'Custom Blocks',
+		],
+	] );
 }
